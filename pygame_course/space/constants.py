831a5f9c946046c7c30f03edd07cfd7edcfa3a9c
msa-sadeghi <sadeghi.msai@gmail.com>
2024-03-17 16:17:32 +0330

    a

---
+++ pygame_course/space/constants.py
@@ -0,0 +1,22 @@
+import pygame
+pygame.init()
+
+SCREEN = pygame.display.set_mode()
+SCREEN_WIDTH = SCREEN.get_width()
+SCREEN_HEIGHT = SCREEN.get_height()
+FPS = 60
+CLOCK = pygame.time.Clock()
+font72 = pygame.font.Font("assets/AttackGraffiti.ttf",72)
+
+
+#############################
+"""
+1. ایجاد فولدر به نام space
+2. داخل این فولدر یک فوادر دیگر به نام assets 
+3. کنار assets
+یک فایل پایتون ایجاد کنید
+constants.py
+
+
+"""
+ 
