d6083a080b36e562fd1756232debf3eb3c6c555c
msa-sadeghi <sadeghi.msai@gmail.com>
2024-06-16 16:04:12 +0330

    a

---
+++ review/1.py
@@ -0,0 +1,22 @@
+# username = input("enter the username: ")
+# password1 = input("enter the password: ")
+# password2 = input("enter the password again: ")
+# if password1 == password2:
+#     print("welcome dear", username)
+# else:
+#     print("password is not correct")
+    
+# numbers = []
+# for i in range(5):
+#     x = int(input("enter a number : "))
+#     numbers.append(x)
+    
+# print(numbers)
+numbers = []
+i = 0
+while i < 5:
+    x = int(input("enter a number: "))
+    numbers.append(x)
+    i += 1
+print(numbers)
+    
---
+++ review/2.py
@@ -0,0 +1,44 @@
+# print(type(1))
+# print(type(1.5))
+# print(type("1.5"))
+
+# print("salaam", "chetori?", "hi", "hello")
+
+# print(1 + 2)
+# print("1" + "2")
+# print("reza" + " sara")
+
+# x = 1
+# x += 1
+# print(x)
+# x = 2
+# print(x)
+# x += 1
+# print(x)
+# x = 2
+# x -= 1
+# print(x)
+# x = 2
+# x *= 2
+# print(x)
+# x = 2
+# x /= 2
+# print(x)
+# x = 2
+# x //= 2
+# print(x)
+# x = 2
+# x **= 2
+# print(x)
+
+
+# print("john" + " "+ "smith")
+# print("john" + " smith")
+name = "john"
+family = "smith"
+message = "his name is {} {}".format(name, family)
+print(message)
+message = f"his name is {name} {family}"
+print(message)
+message = "his name is %s %s"%(name, family)
+print(message)
