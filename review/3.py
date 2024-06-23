150787254be2184b6f121369b114161f65b87d81
msa-sadeghi <sadeghi.msai@gmail.com>
2024-06-23 21:58:56 +0330

    Create 3.py

---
+++ review/3.py
@@ -0,0 +1,16 @@
+import os
+os.system("cls")
+x = int(input("enter a number: "))
+y = int(input("enter a number: "))
+z = None
+try:
+    z = x / y
+except :
+    print("undefined")
+
+print(z)
+print()
+print()
+input("press enter")
+os.system("cls")
+  
