0371670195e7d225d48304c35f8ebd4a191ae72a
msa-sadeghi <sadeghi.msai@gmail.com>
2024-12-08 16:24:25 +0330

    a

--- review/file_manager.py
+++ review/file_manager.py
@@ -34,6 +34,26 @@ def copy_file():
         messagebox.showinfo('خطا', 'فایل با موفقیت کپی نشد')
 
 
+def delete_file():
+    path = file_open_box()
+    try:
+        os.remove(path)
+        messagebox.showinfo('موفق', 'فایل با موفقیت حذف گردید')
+    except:
+        messagebox.showinfo('ناموفق', 'خطا رخ داده است')
+
+def rename_file():
+    try:
+        file = file_open_box()
+        path1 = os.path.dirname(file)
+        extension = os.path.splitext(file)[1]
+        new_name = input("enter the new name:> ")
+        path2 = os.path.join(path1, new_name+ extension)
+        os.rename(file, path2)
+    except:
+        messagebox.showinfo('ناموفق', 'خطا رخ داده است')
+
+
 window = Tk()
 window.title("برنامه من")
 window.configure(bg="black")
@@ -41,5 +61,9 @@ window.geometry("300x380")
 Label(window, text="چه کاری می خواهید انجام دهید؟").pack()
 Button(window, text='بازکردن فایل', command=open_file).pack()
 Button(window, text='کپی فایل', command=copy_file).pack()
+Button(window, text='حذف فایل', command=delete_file).pack()
+Button(window, text='تغییر نام فایل', command=rename_file).pack()
 
 window.mainloop()
+
+
