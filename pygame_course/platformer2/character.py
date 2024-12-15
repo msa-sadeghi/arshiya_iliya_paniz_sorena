6c3ef472961a169e55fee39c2490e02b641802a9
msa-sadeghi <sadeghi.msai@gmail.com>
2024-12-15 16:55:57 +0330

    platformer2

---
+++ pygame_course/platformer2/character.py
@@ -0,0 +1,33 @@
+from pygame import sprite
+import pygame
+import os
+class Character(sprite):
+    def __init__(self, type, x,y, speed, ammo, grenades):
+        self.alive = True
+        self.health = 100
+        self.max_health = 100
+        self.speed = speed
+        self.ammo = ammo
+        self.grenades = grenades
+        self.type = type
+        self.animations_types = ("Idle", "Run","Jump", "Death")
+        self.all_images = {}
+        for animation in self.animations_types:
+            images = []
+            num_of_files = len(os.listdir(f"assets/img/{type}/{animation}"))
+            for i in range(num_of_files):
+                img = pygame.image.load(f"assets/img/{type}/{animation}/{i}.png")
+                img_w = img.get_width()
+                img_h = img.get_height()
+                img = pygame.transform.scale(img, (img_w * 2.5, img_h * 2.5))
+                images.append(img)
+            self.animations_types[animation] = images
+            
+        self.image = self.all_images["Idle"][0]
+        self.rect = self.image.get_rect(topleft = (x,y))
+        self.image_number = 0
+        self.action = "Idle"
+        
+            
+        
+     
---
+++ pygame_course/platformer2/main.py
@@ -0,0 +1,16 @@
+import pygame
+from config import * 
+
+
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+clock = pygame.time.Clock()
+
+
+running = True
+while running == True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+            
+    pygame.display.update()
+    clock.tick(FPS)
