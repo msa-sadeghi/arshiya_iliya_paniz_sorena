76afdc3299074a5ac4769653913befa9759a6991
msa-sadeghi <sadeghi.msai@gmail.com>
2025-02-16 16:56:35 +0330

    s

--- pygame_course/platformer2/level_creator.py
+++ pygame_course/platformer2/level_creator.py
@@ -2,7 +2,7 @@ import pygame
 import pickle
 pygame.init()
 clock = pygame.time.Clock()
-
+from button import Button
 scroll = 0
 scroll_speed = 1
 scroll_left, scroll_right = (False, False)
@@ -29,7 +29,9 @@ def draw_grid():
                          (i * TILE_SIZE - scroll, 0),
                          (i * TILE_SIZE - scroll, SCREEN_HEIGHT)
                          )
-        # TODO draw horintal lines
+        
+    for i in range(ROWS + 1):
+        pygame.draw.line(screen, "white", (0, i * TILE_SIZE), (SCREEN_WIDTH, i * TILE_SIZE))
 def draw_bg():
     screen.fill("green")
     width = bg4.get_width()
@@ -39,7 +41,27 @@ def draw_bg():
         screen.blit(bg1, (i * width - scroll * 0.5,SCREEN_HEIGHT - bg1.get_height() - 150))
         screen.blit(bg2, (i * width - scroll * 0.6,SCREEN_HEIGHT - bg2.get_height()))
         
-            
+images_list = list() 
+for i in range(21):
+    img = pygame.image.load(f"./assets/img/tile/{i}.png")  
+    img = pygame.transform.scale(img, (TILE_SIZE, TILE_SIZE)) 
+    images_list.append(img)       
+
+buttons_list = list()
+col = 0
+row = 0
+for i in range(21):
+    btn = Button(
+        SCREEN_WIDTH + col * 70 + 60,
+        row * 70 + 60,
+        images_list[i]
+    )
+    col += 1
+    if col == 3:
+        col = 0
+        row += 1
+    buttons_list.append(btn)
+
 
 running = True
 while running:
@@ -67,4 +89,8 @@ while running:
         scroll += scroll_speed * 5
     draw_bg()
     draw_grid()
+    pygame.draw.rect(screen, "lightgreen", (SCREEN_WIDTH, 0, SIDE_MARGIN, LOWER_MARGIN + SCREEN_HEIGHT))
+    for i in range(21):
+        buttons_list[i].draw(screen)
+    
     pygame.display.update()
